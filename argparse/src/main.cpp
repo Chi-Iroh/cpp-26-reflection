39a6ea694492378aadbe1cc6423221aef2522f61
Chi-Iroh <69324626+Chi-Iroh@users.noreply.github.com>
2026-05-11 16:30:27 +0200

    For AllowedValues, created a macro to add std::define_static_string

--- argparse/src/main.cpp
+++ argparse/src/main.cpp
@@ -10,8 +10,22 @@
 #include "argparse/include/args_parser.hpp"
 #include "argparse/include/clap.hpp"
 
+// https://www.scs.stanford.edu/~dm/blog/va-opt.html, §The FOR_EACH macro
+#define EXPAND(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) __VA_ARGS__
+
+#define COMMA ,
+#define PARENTHESES ()
+#define FOR_EACH(f, ...) __VA_OPT__(EXPAND(_FOR_EACH(f, __VA_ARGS__)))
+#define _FOR_EACH(f, a, ...) f(a) __VA_OPT__(COMMA FOR_EACH_AGAIN PARENTHESES (f, __VA_ARGS__))
+#define FOR_EACH_AGAIN() _FOR_EACH
+#define STATIC_STRINGS(...) FOR_EACH(std::define_static_string, __VA_ARGS__)
+
 struct Args {
-    [[=clap::AllowedValues<std::define_static_string("hello"), std::define_static_string("world"), std::define_static_string("goodbye")>()]]
+    [[=clap::AllowedValues<STATIC_STRINGS("hello", "world", "goodbye")>()]]
     std::string name;
 
     [[=clap::Between<unsigned int>(1900, 2100)]]
